9f44b48630916d3dcb1cc79cded87797dff4a363
Railly <railly_2009@hotmail.com>
2021-11-13 13:03:45 -0500

    add <Head /> component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "tailwindcss/tailwind.css";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import AppLayout from "components/AppLayout";
 
 export default function MyApp({ Component, pageProps }) {
@@ -7,6 +8,16 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>{`IndieSingles - ${
+          router.pathname === "/"
+            ? "Home"
+            : // routerp.pathname with uppercase first letter
+              router.pathname.split("/")[1].charAt(0).toUpperCase() +
+              router.pathname.split("/")[1].slice(1)
+        }`}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       {router.pathname !== "/login" && router.pathname !== "/register" ? (
         <AppLayout Component={Component} pageProps={pageProps} />
       ) : (
